d8921640f0a5b865fc2e134f9f37143ebb45c709
sahilparikh98 <sparikh98@gmail.com>
2016-08-09 21:52:39 -0700

    update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,23 @@
-
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.afterSave("Bet", function(request) {
+  if(!request.object.get("accepted") && !request.object.get("rejected"))
+  {
+    var query = new Parse.Query(Parse.User)
+    query.equalTo("objectId", request.object.get("receivingUser").id)
+    var pushQuery = new Parse.Query(Parse.Installation)
+    pushQuery.matchesQuery('user', query)
+    Parse.Push.send({
+      where: pushQuery,
+      data: {
+        alert: "You have a bet request from" + request.object.get("receivingUser").username + "!",
+        badge: "Increment"
+      }
+    }, {
+      success: function () {
+        response.sucess("Hello world");
+      },
+      error: function (error) {
+        response.error(error);
+      }
+    });
+  }
 });
