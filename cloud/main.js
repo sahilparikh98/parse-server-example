ca7dd67a5b456f8f6042e47cd64929f0d8eaabc3
sahilparikh98 <sparikh98@gmail.com>
2016-08-11 10:02:01 -0700

    add use master key before push

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.afterSave("Bet", function(request) {
  {
    query = new Parse.Query(Parse.Installation);
    query.equalTo("user", request.object.get("receivingUser"));
-
+   Parse.Cloud.useMasterKey()
    Parse.Push.send({
      where: query,
      data: { alert: "New bet request from " + request.object.get("creatingUser").fetch().get("username") + "!", badge: "Increment"
