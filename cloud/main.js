78d68bd0f3ed21665932b6a078fa76549a5e6297
sahilparikh98 <sparikh98@gmail.com>
2016-08-11 13:21:29 -0700

    add friend request push

--- cloud/main.js
+++ cloud/main.js
@@ -18,3 +18,23 @@ Parse.Cloud.afterSave("Bet", function(request) {
    });
  }
 });
+Parse.Cloud.afterSave("FriendRequest", function(request) {
+ if(!request.object.get("accepted") && !request.object.get("rejected"))
+ {
+   query = new Parse.Query(Parse.Installation);
+   query.equalTo("user", request.object.get("receivingUser"));
+   Parse.Cloud.useMasterKey()
+   Parse.Push.send({
+     where: query,
+     data: { alert: "New friend request from " + request.object.get("senderName") + "!", badge: "Increment"
+           }
+   }, { useMasterKey: true })
+   .then(function() {
+     // Push sent!
+     console.log(request.params);
+     response.success();
+   }, function(error) {
+     // There was a problem :(
+   });
+ }
+});
