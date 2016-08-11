a2109c9b9ee0b07fe51a1238caba8322ad60c341
sahilparikh98 <sparikh98@gmail.com>
2016-08-10 17:40:33 -0700

    update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,21 +1,19 @@
 Parse.Cloud.afterSave("Bet", function(request) {
-  if(!request.object.get("accepted") && !request.object.get("rejected"))
-  {
-    query = new Parse.Query(Parse.Installation);
-    query.equalTo("user", request.object.get("receivingUser"));
-    Parse.Push.send({
-      where: query,
-      data: {
-        alert: "You have a bet request from" + request.object.get("receivingUser").username + "!",
-        badge: "Increment"
-      }
-    }, {
-      success: function () {
-        response.sucess("Hello world");
-      },
-      error: function (error) {
-        response.error(error);
-      }
-    });
-  }
+ if(!request.object.get("accepted") && !request.object.get("rejected"))
+ {
+   query = new Parse.Query(Parse.Installation);
+   query.equalTo("user", request.object.get("toUser"));
+
+   Parse.Push.send({
+     where: query,
+     data: { alert: request.object.get("fromUser") + ': ' + request.object.get("messageText") }
+   }, { useMasterKey: true })
+   .then(function() {
+     // Push sent!
+     console.log(request.params);
+     response.success();
+   }, function(error) {
+     // There was a problem :(
+   });
+ }
 });
