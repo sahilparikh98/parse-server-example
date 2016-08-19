e1f95fa4e24dcb3a7aa14b9274e5aad5a421a173
Sahil Parikh <sparikh98@gmail.com>
2016-08-19 03:13:11 -0500

    switch

--- cloud/main.js
+++ cloud/main.js
@@ -1,33 +1,14 @@
-Parse.Cloud.afterSave("Bet", function(request) {
- if(!request.object.get("accepted") && !request.object.get("rejected") && !request.object.get("finished"))
- {
-   query = new Parse.Query(Parse.Installation);
-   query.equalTo("user", request.object.get("toUser"));
-   Parse.Cloud.useMasterKey();
-   Parse.Push.send({
-     where: query,
-     data: { alert: "New bet request from " + request.object.get("senderName") + "!", badge: "Increment"
-           }
-   }, { useMasterKey: true })
-   .then(function() {
-     // Push sent!
-     console.log(request.params);
-     response.success();
-   }, function(error) {
-     // There was a problem :(
-   });
- }
-});
+
 
 Parse.Cloud.afterSave("Bet", function(request) {
  if(!request.object.get("accepted") && !request.object.get("rejected") && !request.object.get("finished"))
  {
    query = new Parse.Query(Parse.Installation);
-   query.equalTo("user", request.object.get("creatingUser"));
+   query.equalTo("user", request.object.get("receivingUser"));
    Parse.Cloud.useMasterKey();
    Parse.Push.send({
      where: query,
-     data: { alert: "" + request.object.get("receiverName") + " has accepted your bet!", badge: "Increment"
+     data: { alert: "New bet request from " + request.object.get("senderName") + "!", badge: "Increment"
            }
    }, { useMasterKey: true })
    .then(function() {
