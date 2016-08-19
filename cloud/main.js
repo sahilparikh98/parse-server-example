7d3e1648e3988bdbe426a59a046dc2ad56fc860b
Sahil Parikh <sparikh98@gmail.com>
2016-08-19 03:16:01 -0500

    add accept notification back

--- cloud/main.js
+++ cloud/main.js
@@ -21,6 +21,27 @@ Parse.Cloud.afterSave("Bet", function(request) {
  }
 });
 
+Parse.Cloud.afterSave("Bet", function(request) {
+ if(request.object.get("accepted") && !request.object.get("rejected") && !request.object.get("finished"))
+ {
+   query = new Parse.Query(Parse.Installation);
+   query.equalTo("user", request.object.get("creatingUser"));
+   Parse.Cloud.useMasterKey();
+   Parse.Push.send({
+     where: query,
+     data: { alert: "" + request.object.get("receiverName") + " has accepted your bet!", badge: "Increment"
+           }
+   }, { useMasterKey: true })
+   .then(function() {
+     // Push sent!
+     console.log(request.params);
+     response.success();
+   }, function(error) {
+     // There was a problem :(
+   });
+ }
+});
+
 Parse.Cloud.afterSave("FriendRequest", function(request) {
  if(!request.object.get("accepted") && !request.object.get("rejected"))
  {
